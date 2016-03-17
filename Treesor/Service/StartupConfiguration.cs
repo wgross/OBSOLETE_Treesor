73cb5e7603560c2d8a97c84ebbe85bea124b46a0
Wolfgang Groß <bitbucket@text-mode.de>
2016-03-17 21:25:44 +0100

    service hull is running

---
+++ Treesor/Service/StartupConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using NLog;
+using Owin;
+using Swashbuckle.Application;
+using System.Web.Http;
+
+namespace Treesor.Service
+
+{
+    public sealed class StartupConfiguration
+    {
+        private static readonly Logger log = LogManager.GetCurrentClassLogger();
+
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            //ODataModelBuilder builder = new ODataConventionModelBuilder();
+            //builder.EntitySet<Entity>("Entities");
+
+            config.MapHttpAttributeRoutes();
+
+            //config.MapODataServiceRoute(
+            //    routeName: "ODataRoute",
+            //    routePrefix: "odata",
+            //    model: builder.GetEdmModel());
+
+            config
+                .EnableSwagger(c =>
+                {
+                    c.SingleApiVersion("v1", "Treesor");
+                    //c.CustomProvider(defaultProvider => new ODataSwaggerProvider(defaultProvider, c, config));
+                })
+                .EnableSwaggerUi();
+
+            //config.EnsureInitialized();
+            //config.Routes.ToList().ForEach(r => log.Info().Message($"Route: {r.GetVirtualPath().Route}").Write());
+            //appBuilder.UseWebApi(config);
+        }
+    }
+}
